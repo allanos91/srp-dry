9bc7954e3cef378ce323c14ba0500af2dc8267dd
Vernon Leadbeater <vernon.leadbeater@gmail.com>
2023-10-03 23:30:23 -0400

    alternative

--- practice.js
+++ practice.js
@@ -1,45 +1,30 @@
+// function biggerFunc(num1, num2, isBigger, func) {
+//   let chosenNum;
+//   if (isBigger ? num1 > num2 : num1 < num2) {
+//     chosenNum = num1;
+//   } else {
+//     chosenNum = num2;
+//   }
+//   return func(chosenNum)
+// }
+
+const biggerFunc = (num1, num2, isLarger, func) => 
+  func(isBigger ? Math.max(num1, num2) : Math.min(num1, num2));
+
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  return biggerFunc(num1, num2, true, bigNum => bigNum * 2)
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return biggerFunc(num1, num2, true, bigNum => bigNum / 3)
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  return biggerFunc(sum1, sum2, true, bigNum => `I ate ${bigNum} tacos.`)
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  return biggerFunc(weight1, weight2, false, smallDog => `I adopted a dog that weighs ${smallDog} pounds.`)
 }
 
 
